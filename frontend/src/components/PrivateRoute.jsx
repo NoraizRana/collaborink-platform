e90db1b49c671aa899775fb048ff16acbeca896b
Noraiz Rana <noraizrana389@gmail.com>
2026-04-25 21:47:31 +0500

    Basic files added

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,4 +1,4 @@
-import { useAuthStore } from '../store/authStore'
+// import { useAuthStore } from '../store/authStore'
 import { Navigate } from 'react-router-dom'
 
 export default function PrivateRoute({ children }) {
